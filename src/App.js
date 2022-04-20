ba1248428b6154bc8b65d31378d59ed8140f01c4
taha sajid <tahapk5875@gmail.com>
2022-04-20 15:44:49 +0500

    check responsiveness

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <TopNav />
+      {/* <TopNav /> */}
       <Header />
       <RecentlySoldSlider />
       <Buyers />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { NavLogo } from "./Svgs";
 
 const Navbar = () => {
+  const [isNavActive, setIsNavActive] = useState(false);
+
   return (
     <div className="main-nav">
-      <nav className="nav-toggle">
+      <nav
+        className={isNavActive ? "nav-toggle nav-active" : "nav-toggle"}
+        onClick={() => {
+          setIsNavActive(true);
+          console.log("click");
+        }}
+      >
         <button className="hamburger">
           <div className="line1"></div>
           <div className="line2"></div>
         </button>
       </nav>
-      <div className="nav-logo">
-        <NavLogo className="logo-mobile" />
+      <div className="nav-content">
+        <div className="nav-logo">
+          <NavLogo className="logo-mobile" />
 
-        <h4>
-          Investors<span>club </span>
-        </h4>
-        <a>buy</a>
-        <a>sell</a>
-      </div>
-      <div className="nav-btn">
-        <button className="login">login</button>
-        <button className="sign-up">sign up</button>
+          <h4>
+            Investors<span>club </span>
+          </h4>
+          <a
+          // style={
+          //   isNavActive ? `${{ display: "block" }}` : `${{ display: "none" }}`
+          // }
+          >
+            buy
+          </a>
+          <a>sell</a>
+        </div>
+        <div className="nav-btn">
+          <button className="login">login</button>
+          <button className="sign-up">sign up</button>
+        </div>
       </div>
     </div>
   );
