89bd9bf91b5ca3a45a654078ef2d8d8c8eeca940
Syed Muhammad Taha <tahapk58752@gmail.com>
2024-05-24 08:05:27 +0500

    added vercel.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
- 
+
 // If you want to start measu ring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
